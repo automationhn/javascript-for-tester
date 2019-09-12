3765d6166052010cc7e910e92e3e27750e432aba
Nhat Minh Nguyen <nhatmnguyen@kms-technology.com>
2019-09-12 11:40:15 +0700

    Adding new practice and refactoring

---
+++ practices/day_2.js
@@ -0,0 +1,29 @@
+/*
+1. Tạo 1 đối tượng (object) nhân viên gồm name, birthDate, gender
+Gợi ý: 
+{
+    name: hanh,
+    gender: female,
+    birthDate: '1990-06-24'
+}
+
+2. Thêm thuộc tính (field/property) vào cho đối tượng trên với kiểu dữ liệu là mảng để thể hiện skill của người này
+Gợi ý: thêm field { skill: ['javascript', 'testing', 'automationUI'] }
+
+3. Xoá thuộc tính name và thêm vào thuộc tính fullName
+
+4. Dựa vào thuộc tính gender, thêm thuộc tính isMale có giá trị true/false
+
+5. In ra danh sách những số lẻ có 2 chữ số chia hết cho 3 và 9 (*)
+)
+
+6. In ra danh sách những số có index chẵn
+
+6. Tính tổng của những số trên theo 2 cách (1 cách dùng hàm reduce, cách còn lại thoải mái)
+
+7. Sắp xếp dãy số kể trên theo thứ tự giảm dần
+
+8. Tính tích của 2 số lớn nhất và nhỏ nhất của dãy số trên
+
+9. Với kết quả của (*) lấy ra những số chỉ chia hết cho 3 và sắp xếp theo thứ tự giảm dần
+*/
---
+++ utils/calculateAverageScore.js
@@ -0,0 +1,4 @@
+export default function(scores) {
+    const { math, word, excel } = scores
+    return Math.round((math + word + excel)/3, 2);
+}
