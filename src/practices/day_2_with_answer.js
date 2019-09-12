682665d68896661b69f67d870ac4d6882ac1d78f
Nhat Minh Nguyen <nhatmnguyen@kms-technology.com>
2019-09-12 15:32:39 +0700

    Adding the awnser of day_2

---
+++ src/practices/day_2_with_answer.js
@@ -0,0 +1,82 @@
+import printArray from '../utils/printArray';
+/*
+1. Tạo 1 đối tượng (object) nhân viên gồm name, birthDate, gender
+Gợi ý: 
+{
+    name: hanh,
+    gender: female,
+    birthDate: '1990-06-24'
+}
+*/
+const employee = {
+    name: 'hanh',
+    gender: 'female',
+    birthDate: '1990-06-24'
+};
+/*
+
+2. Thêm thuộc tính (field/property) vào cho đối tượng trên với kiểu dữ liệu là mảng để thể hiện skill của người này
+Gợi ý: thêm field { skill: ['javascript', 'testing', 'automationUI'] }
+*/
+Object.assign(employee, { skill: ['javascript', 'testing', 'automationUI'] });
+/*
+
+3. Xoá thuộc tính name và thêm vào thuộc tính fullName
+*/
+delete employee.name;
+Object.assign(employee, { fullName: 'Nguyen Thi Duc Hanh' });
+/*
+
+4. Dựa vào thuộc tính gender, thêm thuộc tính isMale có giá trị true/false
+*/
+Object.assign(employee, { isMale: employee.gender === 'male' });
+/*
+
+5. In ra danh sách những số lẻ có 2 chữ số chia hết cho 3 và 9 (*)
+*/
+const listNumber = [];
+for(let i = 0; i < 100; i++) {
+    if( i%2 === 1 && (i%3 === 0 || i%9 === 0)) {
+        listNumber.push(i);
+        console.log(i);
+    }
+}
+
+/*
+
+6. In ra danh sách những số có index chẵn trong dãy số (*)
+*/
+for(let i = 0; i < listNumber.length; i+=2) {
+    console.log(listNumber[i]);
+}
+/*
+
+7. Tính tổng của dãy số (*) theo 2 cách (1 cách dùng hàm reduce, cách còn lại thoải mái)
+*/
+let sum1 = 0;
+listNumber.forEach(i => { sum1 += i });
+console.log('Tong bang: ' + sum1);
+
+let sum2 = listNumber.reduce((sum, i) => {
+    return sum += i;
+}, 0);
+console.log(`Tong bang: ${sum2}`);
+/*
+
+8. Sắp xếp dãy số kể trên theo thứ tự giảm dần
+*/
+listNumber.sort((a, b) => { return b - a; });
+printArray(listNumber);
+/*
+
+9. Tính tích của 2 số lớn nhất và nhỏ nhất của dãy số trên
+*/
+const min = listNumber[listNumber.length - 1];
+const max = listNumber[0];
+console.log(`Tich dau duoi: ${min * max}`);
+/*
+
+10. Với kết quả của (*) lấy ra những số chỉ chia hết cho 3 và sắp xếp theo thứ tự giảm dần
+*/
+const a = listNumber.filter((i) => { return i%9 !== 0; }).sort((a, b) => { return b - a; });
+printArray(a);
