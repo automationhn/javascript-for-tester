b1c0447cfac4b1c4951b1f8f0041b17e0bcbd9bb
Nhat Minh Nguyen <nhatmnguyen@kms-technology.com>
2019-09-13 10:47:45 +0700

    Adding practice for day_3

---
+++ src/practices/day_3.js
@@ -0,0 +1,42 @@
+/*
+1. In ra danh sách những số lẻ có 2 chữ số chia hết cho 3 và 9 (*)
+
+2. In ra danh sách những số có index chẵn của dãy số (*)
+
+3. Tính tổng của dãy số (*) theo 2 cách (1 cách dùng hàm reduce, cách còn lại thoải mái)
+
+4. Sắp xếp dãy số (*) kể trên theo thứ tự giảm dần
+
+5. Tính tích của 2 số lớn nhất và nhỏ nhất của dãy số (*)
+
+6. Với kết quả của dãy số (*) lấy ra những số chỉ chia hết cho 3 và sắp xếp theo thứ tự giảm dần
+*/
+
+/*
+7. Cho mảng sau:
+const array = [5, -3, 12, 9, -2, 56, 24, 83, 0] (*)
+
+a. Sắp xếp mảng trên theo thứ tự tăng dần và giảm dần
+
+b. Tìm index của số chẵn lớn nhất trong mảng (*)
+    Gợi ý: filter để lấy những số chẵn của mảng, sau đó tìm số lớn nhất, dùng giá trị đó tìm index của nó
+
+c. Tính tổng của những số âm trong mảng (*)
+*/
+
+/*
+8.1 Viết 1 function để tính tổng của 2 tham số truyền vào
+Gợi ý: truyền 2 tham số a = 4, b = 8 -> In ra màn hình kết quả 12
+
+8.2 Viết 1 function để tính các phương thức "+ = x :" của 2 tham số truyền vài\o
+Gợi ý: truyền 2 tham số a = 8, b = 2 -> In ra màn hình object kết quả với nội dung như sau
+{
+    tong: 10,
+    hieu : 6,
+    tich: 16,
+    thương: 4
+}
+
+8.3 Viết hàm(function) để tạo ra tên đầy đủ của 1 người, 3 tham số truyền vào là tên, họ, lót
+Gợi ý: function(ho, tên, lot) -> "Họ Lót Tên"
+*/
