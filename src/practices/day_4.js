0348cd207d0eafbb500bbc2e7f2c41d43f17332d
Nhat Minh Nguyen <nhatmnguyen@kms-technology.com>
2019-10-03 18:00:11 +0700

    Adding pratice for day 4

---
+++ src/practices/day_4.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable max-len */
+/*
+1. Tạo ra 1 mảng số tự nhiên bé hơn 200 với chữ số hàng đơn vị là 1
+ex: array1 = [1, 11, 21, 31, ...]
+
+2. Tạo ra mảng mới từ mảng cũ bằng cách cộng 2 phần tử liên tiếp lại với nhau
+ex: array2 = [12, 52, ...] 12 = 1 + 11; 52 = 21 + 31
+
+3. Tính tổng của 2 số lớn nhất từ array1 và array2
+
+4. Tạo ra mảng hiển thị danh sách tiện nghi phòng nghỉ và sắp xếp theo thứ tự alphabet và ngược lại
+ex: tien_nghi = ['TV', 'may lanh', 'mini-bar', 'lo suoi', 'bon tam', 'phong tam dung']
+
+5. Xoá tiện nghi có độ dài lớn nhất trong danh sách tiện nghi
+ex: 
+  - 'ti vi' có độ dài 2
+  - 'may lanh' có độ dài là 7
+
+6. Tạo 1 đối tượng định nghĩa cho phòng nghỉ của khách sạn 
+ex: 
+var phong = {
+  id: 1,
+  so_nguoi: 4,
+  loai_phong: 'luxury',
+  dia_diem: 'Cantho'
+}
+
+7. Thêm những thuộc tính về tiện nghi và đối tượng vừa tạo
+
+8. Xoá thuộc tính dia_diem ra khỏi đối tượng 
+
+9. Thay thế thuộc tính so_nguoi bằng co_giuong với giá tri 'kingsize'
+
+10. Viết phương thức in ra thông tin phòng từ đối tượng phòng theo định đạng sau:
+ex: 1_luxury_kingsize_Cantho
+*/
