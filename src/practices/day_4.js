7cc938fd90a7b26e72ef60d7e420b79797a00f08
Nhat Minh Nguyen <nhatmnguyen@kms-technology.com>
2019-10-05 14:07:07 +0700

    Updating practice for day 4 and 5

--- src/practices/day_4.js
+++ src/practices/day_4.js
@@ -1,46 +1,44 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable max-len */
 /*
+
+MẢNG - ARRAY
+
 1. Tạo ra mảng số tự nhiên chia hết cho 5 trong khoảng [-200, 200]
 
 2. Cho mảng array1 = [0, 1]
-Tạo ra các phần tử tiếp theo bằng cách cộng 2 phần từ liền trước lại và thêm vào mảng sao cho phần tử cuối cùng phải có 3 kí tự
+Tạo ra các phần tử tiếp theo bằng cách cộng 2 phần từ liền trước lại và thêm vào mảng sao cho phần tử cuối cùng là số có 3 chữ số
 0 + 1 = 1
 1 + 1 = 2
 1 + 2 = 3
 ex: array1 = [0, 1, 1, 2, 3, 5, 8, 13, ...]
+note: có thể dùng for, while hoặc do while
 
 3. Cho mảng array2 = [1, 2]
-Tạo ra các phần tử tiếp theo bằng cách nhân 2 phần từ liền trước lại và thêm vào mảng sao cho phần tử cuối cùng phải có 8 chữ số
+Tạo ra các phần tử tiếp theo bằng cách nhân 2 phần từ liền trước lại và thêm vào mảng sao cho phần tử cuối cùng là số có 4 chữ số
 1 * 2 = 2
 2 * 2 = 4
 1 + 2 = 3
 ex: array2 = [1, 2, 2, 4, 8, 32, ...]
+note: có thể dùng for, while hoặc do while
 
-4. Tạo ra mảng hiển thị danh sách tiện nghi phòng nghỉ và sắp xếp theo thứ tự alphabet và ngược lại
-ex: tien_nghi = ['TV', 'may lanh', 'mini-bar', 'lo suoi', 'bon tam', 'phong tam dung']
+4. Nối 2 mảng lại với nhau
+note: có thể dùng mọi cách
 
-5. Xoá tiện nghi có độ dài lớn nhất trong danh sách tiện nghi
-ex: 
-  - 'ti vi' có độ dài 2
-  - 'may lanh' có độ dài là 7
+5. Loại bỏ những phần tử trùng nhau trong mảng mới vừa nối lại
+note: có thể dùng google để tìm cách, nên tìm bằng tiếng anh, hạn chế sử dụng tiếng việt nha e, google cũng là 1 skill trong lập trình
+syntax: javascript distict duplication element in array ...
 
-6. Tạo 1 đối tượng định nghĩa cho phòng nghỉ của khách sạn 
-ex: 
-var phong = {
-  id: 1,
-  so_nguoi: 4,
-  loai_phong: 'luxury',
-  dia_diem: 'Cantho'
+6. Viết phương thức với tham số truyền vào là 1 mảng bất kì và trả về tổng giá trị các phần tử trong mảng đó
+
+7. Viết phương thức với tham số truyền vào là 1 mảng bất kì và trả về phần tử nhỏ nhất trong mảng đó
+
+8. Viết phương thức với tham số truyền vào là 1 mảng bất kì và trả về 1 đối tượng chứa phần tử lớn và nhỏ nhất trong mảng
+ex: {
+  min: -16,
+  max: 24 
 }
 
-7. Thêm những thuộc tính về tiện nghi vào đối tượng vừa tạo
+***9. Thử viết hàm test cho 3 phương thức ở câu (6, 7, 8)
 
-8. Xoá thuộc tính dia_diem ra khỏi đối tượng 
-
-9. Thay thế thuộc tính so_nguoi bằng co_giuong với giá tri 'kingsize'
-
-10. Viết phương thức in ra thông tin phòng từ đối tượng phòng theo định đạng sau:
-ex: 1_luxury_kingsize_Cantho
-note: có thể truyền tham số theo 1 danh sách hoặc truyền cả đối tương phòng nghỉ vào phương thức vừa tạo
 */
