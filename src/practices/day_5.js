ce103b5b89cc58ad1220ba9343010e724e6882a2
Nhat Minh Nguyen <nhatmnguyen@kms-technology.com>
2019-10-06 11:50:42 +0700

    Update question

--- src/practices/day_5.js
+++ src/practices/day_5.js
@@ -2,17 +2,24 @@
 /* eslint-disable no-trailing-spaces */
 /*
 
-ĐỐI TƯƠNG - OBJECT
+---> MẢNG - ARRAY <---
 
 1. Tạo ra mảng hiển thị danh sách tiện nghi phòng nghỉ và sắp xếp theo thứ tự alphabet và ngược lại
-ex: tien_nghi = ['TV', 'may lanh', 'mini-bar', 'lo suoi', 'bon tam', 'phong tam dung']
+ex: tien_nghi = ['ti vi', 'may dieu hoa', 'mini-bar', 'lo suoi am', 'bon tam', 'may chieu phim', 'phong tam dung']
 
 2. Xoá tiện nghi có độ dài lớn nhất trong danh sách tiện nghi
 ex: 
   - 'ti vi' có độ dài 5
   - 'may lanh' có độ dài là 8
 
-3. Tạo 1 đối tượng định nghĩa cho phòng nghỉ của khách sạn 
+3**. Sắp xếp mảng còn lại theo thứ tự tăng dần của độ dài mỗi phần tử
+ex: tien_nghi_1 = ['ti vi', 'bon tam', 'may dieu hoa', ...]
+
+4. Với mảng đã sắp xếp trên, loại bỏ phần tử đầu và cuối mảng
+
+---> ĐỐI TƯƠNG - OBJECT <---
+
+5. Tạo 1 đối tượng định nghĩa cho phòng nghỉ của khách sạn 
 ex: 
 var phong = {
   id: 1,
@@ -21,13 +28,16 @@ var phong = {
   dia_diem: 'Cantho'
 }
 
-4. Thêm những thuộc tính về tiện nghi vào đối tượng vừa tạo
+6. Thêm những thuộc tính về tiện nghi vào đối tượng vừa tạo
 
-5. Xoá thuộc tính dia_diem ra khỏi đối tượng 
+7. Xoá thuộc tính dia_diem ra khỏi đối tượng 
 
-6. Thay thế thuộc tính so_nguoi bằng co_giuong với giá tri 'kingsize'
+8. Thay thế thuộc tính so_nguoi bằng co_giuong với giá tri 'kingsize'
 
-7. Viết phương thức in ra thông tin phòng từ đối tượng phòng theo định đạng sau:
+9. Viết phương thức in ra thông tin phòng từ đối tượng phòng theo định đạng sau:
 ex: 1_luxury_kingsize_Cantho
 note: có thể truyền tham số theo 1 danh sách hoặc truyền cả đối tương phòng nghỉ vào phương thức vừa tạo
+
+10. Với phương thức đã viết ở câu 9, sửa lại để xuất ra thêm danh sách tiện nghi đc nối với nhau bằng dấu ';'
+ex: 1_luxury_kingsize_Cantho_tivi;bontam;maydieuhoa
 */
