036510203ac112dd743929dbf2323b862a07c4e5
hanhtdnguyen <hanhtdnguyen@FVFXJ2K4HV2H.local>
2019-10-21 21:27:46 +0700

    day 7

--- src/practices/day_7.js
+++ src/practices/day_7.js
@@ -2,6 +2,8 @@
 /* eslint-disable max-len */
 /* eslint-disable no-trailing-spaces */
 
+import logger, { newLine, newLineWithDash } from '../utils/logger';
+
 const number = [5, 211, 43, 24, 1108, 246, 33, 81, 599, 11, 35, 98, 56, 104, 65];
 
 /*
@@ -13,6 +15,29 @@ const number = [5, 211, 43, 24, 1108, 246, 33, 81, 599, 11, 35, 98, 56, 104, 65]
 ...
 - số cuối cùng là: 1108
 */
+
+//Cau 1
+function timSoChanLonHon50 (arr){
+  const arr1 = arr.filter((ele) => { 
+  return ele % 2 === 0 && ele > 50;});
+  const arr2 = arr1.sort((a,b) => {return a - b;});
+  return arr2;
+};
+const numberNew = timSoChanLonHon50(number);
+logger(`So chan lon hon 50 la: ${numberNew.join(', ')}`);
+
+//cau 2
+for (let i = 0; i < numberNew.length; i++){
+  let a = numberNew[i];
+  if (i < numberNew.length-1){  
+    logger (`So thu ${i+1} la: ${a}`);
+  }
+  else {
+    logger (`So cuoi cung la: ${a}`);
+  }
+}
+  
+  
 
 const teams = [
   {
@@ -44,12 +69,62 @@ const teams = [
     members: ['Chanh', 'Cuong', 'Hao Hach'],
   },
 ];
-
+newLineWithDash();
 /*
-3. Viết function truyền vào mảng teams -> trả ra mảng chứa danh sách tên của tất cả các team
-4. Viết function tuyền vào mảng teams -> trả về tên của team có qa_lead là chị của C5
-5. Viết function tuyền vào mảng teams và tên team -> trả về danh sách thành viên đc sắp xếp theo thứ tự tăng dần của team được truyền vào
-6. Viết function tuyền vào mảng teams và trả về team có số thành viên đông nhất
+
 
 7. Viết function tuyền vào mảng teams -> trả về danh sách tất cả các thành viên có trong team mà dev_lead có tên bắt đầu bằng chữ H
 */
+
+//3. Viết function truyền vào mảng teams -> trả ra mảng chứa danh sách tên của tất cả các team
+logger('cau 3');
+function allTeams(data){
+  const teamArr = [];
+  data.forEach((elemment) => {
+    const team = elemment.name;
+    teamArr.push(team);
+  });
+  return teamArr;
+}
+const danhSachTeam = allTeams(teams);
+logger(danhSachTeam);
+newLineWithDash();
+
+//4. Viết function tuyền vào mảng teams -> trả về tên của team có qa_lead là chị của C5
+logger('cau 4');
+function qaLead(data, name){
+  const crazy = data.find((ele) => {
+    return ele.qa_lead === name;
+  });
+ return crazy.name;
+}  
+
+const chiCuaC5 = qaLead(teams,"C3");
+logger(`team có qa_lead là chị của C5 la : ${chiCuaC5}`);
+newLineWithDash();
+
+//5. Viết function tuyền vào mảng teams và tên team -> trả về danh sách thành viên đc sắp xếp theo thứ tự tăng dần của team được truyền vào
+logger('cau 5');
+function danhSachThanhVien (data, name){
+  const memByTeam = data.find((ele) => {  
+      const a = ele.members;
+      const b = a.sort();
+      return b; 
+    }
+  );
+  return memByTeam.members;
+}
+
+const memOfTeam = danhSachThanhVien(teams, "Salad")
+logger(memOfTeam);
+newLineWithDash();
+
+//6. Viết function tuyền vào mảng teams và trả về team có số thành viên đông nhất
+logger('cau 6');
+function largestTeam(data){
+  const sortLargestTeam = data.sort((a, b) => { return b.members.length - a.members.length; });
+  return sortLargestTeam[0]; 
+}
+const theLargestTeam = largestTeam(teams);
+logger(theLargestTeam);
+newLineWithDash();
