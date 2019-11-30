3fc599083641bad2444b7e17e88ed1c755acfe38
Nhat Minh Nhat <nhat.nguyen@coxautoinc.com>
2019-11-30 15:05:25 +0700

    Adding template test function

--- src/__test__/function_test.spec.js
+++ src/__test__/function_test.spec.js
@@ -7,8 +7,9 @@ describe('testCalculation', () => {
   describe('calculateAverageScore()', () => {
     it('should return right', () => {
 
-      let score = { math: 9, word: 9, exel: 9 };
+      let score = { math: 9, word: 9, excel: 9 };
       var actual = calculateAverageScore(score);
+      console.log(actual);
 
       assert.equal(actual, 9, 'The result should be matched');
     });
