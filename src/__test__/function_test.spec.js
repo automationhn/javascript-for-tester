ef95a738a3e563673ae08773e9a51cdb77c85a68
hanhtdnguyen <hanhtdnguyen@kms-technology.com>
2019-11-30 22:42:53 +0700

    commit_file_3011

--- src/__test__/function_test.spec.js
+++ src/__test__/function_test.spec.js
@@ -1,17 +1,45 @@
 /* eslint-disable no-undef */
 const assert = require('assert');
-// const calculateAverageScore = require('../utils/calculateAverageScore');
 import calculateAverageScore from '../utils/calculateAverageScore';
 
 describe('testCalculation', () => {
   describe('calculateAverageScore()', () => {
-    it('should return right', () => {
-
+    it('should return right 1', () => {
+      // A - A - A
+      // Arrange
       let score = { math: 9, word: 9, excel: 9 };
-      var actual = calculateAverageScore(score);
-      console.log(actual);
+      const expect = 9;
 
-      assert.equal(actual, 9, 'The result should be matched');
+      // Action
+      const actual = calculateAverageScore(score);
+
+      // Assert
+      assert.equal(actual, expect, 'The result should be matched');
     });
+
+    it('should return error when the expected average score is not equal', () => {
+      // Arrange
+      let score = { math: 9, word: 9, excel: 9 };
+      const expect = 10;
+
+      // Action
+      const actual = calculateAverageScore(score);
+
+      // Assert
+      assert.notEqual(actual, expect, 'The result should not be matched');
+    });
+
+    it('should return message when missing one of score', () => {
+      // Arrange      
+      let score = { word: 9, excel: 9 };
+      const expect = 0;
+
+      // Action
+      const actual = calculateAverageScore(score);
+
+      // Assert
+      assert.equal(actual, expect, 'The data is not matched');
+    });
+
   });
 });
--- src/utils/calculateAverageScore.js
+++ src/utils/calculateAverageScore.js
@@ -1,4 +1,12 @@
+import logger from "./logger";
+
 export default function (scores) {
+  if (scores === undefined) {
+    return 0;
+  } 
   const { math, word, excel } = scores;
-  return Math.round((math + word + excel) / 3, 2);
+  if (typeof math === "number" && typeof word === "number" && typeof excel === "number") {
+    return Math.round((math + word + excel) / 3, 2);
+  }
+  return 0;
 }
